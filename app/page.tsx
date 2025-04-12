de97a55716912165e5045c0f5429edb5c3badbb7
pritamawatade <pritamawatade5@gmail.com>
2025-04-13 00:55:19 +0530

    Remove unused components and files, update registry with new button and navbar components

--- app/page.tsx
+++ app/page.tsx
@@ -1,66 +1,7 @@
-import * as React from "react"
-import { OpenInV0Button } from "@/components/open-in-v0-button"
-import { HelloWorld } from "@/registry/new-york/hello-world/hello-world"
-
-import Button from "@/registry/new-york/button/button"
-import Navbar from "@/registry/new-york/navbar/navbar"
-
-// This page displays items from the custom registry.
-// You are free to implement this with your own design as needed.
-
 export default function Home() {
   return (
-    <div className="max-w-3xl mx-auto flex flex-col min-h-svh px-4 py-8 gap-8">
-      <Navbar />
-      <Button variant="primary" size="medium" >
-        Primary
-      </Button>
-      <Button variant="secondary" size="medium" >
-        Secondary
-      </Button>
-      <Button variant="danger" size="medium" >
-        Danger
-      </Button>
-      <Button variant="success" size="medium" >
-        Success
-      </Button>
-      <main className="flex flex-col flex-1 gap-8">
-        <div className="flex flex-col gap-4 border rounded-lg p-4 min-h-[450px] relative">
-          <div className="flex items-center justify-between">
-            <h2 className="text-sm text-muted-foreground sm:pl-3">
-              A simple hello world component
-            </h2>
-            <OpenInV0Button name="hello-world" className="w-fit" />
-          </div>
-          <div className="flex items-center justify-center min-h-[400px] relative">
-            <HelloWorld />
-          </div>
-        </div>
-
-        {/* <div className="flex flex-col gap-4 border rounded-lg p-4 min-h-[450px] relative">
-          <div className="flex items-center justify-between">
-            <h2 className="text-sm text-muted-foreground sm:pl-3">
-              A contact form with Zod validation.
-            </h2>
-            <OpenInV0Button name="example-form" className="w-fit" />
-          </div>
-          <div className="flex items-center justify-center min-h-[500px] relative">
-            <ExampleForm />
-          </div>
-        </div> */}
-{/* 
-        <div className="flex flex-col gap-4 border rounded-lg p-4 min-h-[450px] relative">
-          <div className="flex items-center justify-between">
-            <h2 className="text-sm text-muted-foreground sm:pl-3">
-              A complex component showing hooks, libs and components.
-            </h2>
-            <OpenInV0Button name="complex-component" className="w-fit" />
-          </div>
-          <div className="flex items-center justify-center min-h-[400px] relative">
-            <PokemonPage />
-          </div>
-        </div> */}
-      </main>
-    </div>
+    <>
+    <h1>Coming soon....</h1>
+    </>
   )
 }
--- registry/new-york/hello-world/hello-world.tsx
+++
@@ -1,3 +0,0 @@
-export function HelloWorld() {
-  return <h1 className="text-2xl font-bold">Hello lazyui</h1>
-}
