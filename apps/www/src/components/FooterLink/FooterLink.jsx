7fb68819b51fe78ac4e17d16ae873aa57842abdd
Pritam <117350475+Pritamawatade@users.noreply.github.com>
2025-04-20 17:55:55 +0530

    Update FooterLink.jsx

--- apps/www/src/components/FooterLink/FooterLink.jsx
+++ apps/www/src/components/FooterLink/FooterLink.jsx
@@ -2,7 +2,7 @@ import { motion } from "framer-motion";
 
 function FooterLink({ text, link }) {
   return (
-    <motion.a href={#${text.toLowerCase()}} className="hover:text-white transition-colors" whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.98 }}>
+    <motion.a href={link} className="hover:text-white transition-colors" whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.98 }}>
       {text}
     </motion.a>
   );
