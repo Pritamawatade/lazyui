ccea1f7599401b0996ac6cacd8c57324b722d480
Pritam <117350475+Pritamawatade@users.noreply.github.com>
2025-04-20 18:24:14 +0530

    Update main.jsx

--- apps/www/src/main.jsx
+++ apps/www/src/main.jsx
@@ -9,7 +9,6 @@ createRoot(document.getElementById("root")).render(
    <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
